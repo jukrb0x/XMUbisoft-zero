da646248227fb359791db8936bc62fd29e626454
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2021-10-21 11:45:27 +0800

    Tilemap: add basic level 3 tilemap element

---
+++ Assets/Scripts/RubyController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+    private float _horizontal;
+    private float _vertical;
+    private const float VelocityFraction = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 30;
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = _rigidbody2D.position;
+        position.x += VelocityFraction * _horizontal * Time.deltaTime;
+        position.y += VelocityFraction * _vertical * Time.deltaTime;
+        _rigidbody2D.MovePosition(position);
+    }
+
+}
