e92f11d0e37529f5481db215386fbfdcad426ba6
GDistories <18663977305@163.com>
2021-10-24 09:16:40 +0800

    Add base  movement and camera

---
+++ Assets/Scripts/Component/CharacterRun.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterRun : CharacterComponents
+{
+    [SerializeField] private float runSpeed = 10f;
+
+    protected override void HandleInput()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Run();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            StopRun();
+        }
+    }
+
+    private void Run()
+    {
+        characterMovement.MoveSpeed = runSpeed;
+    }
+    private void StopRun()
+    {
+        characterMovement.ResetSpeed();
+    }
+}
