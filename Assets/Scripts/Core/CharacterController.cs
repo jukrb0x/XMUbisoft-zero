00020f9f1553a55525a5a5498ae4f3de5dd66ebe
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2021-10-26 23:03:09 +0800

    fix(scripts): organize base scripts

--- Assets/Scripts/Core/CharacterController.cs
+++ Assets/Scripts/Core/CharacterController.cs
@@ -1,51 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+namespace Core
 {
-    //[SerializeField] private float speed = 20f;  //REMOVE it, because we no longer use it anymore
-
-    // Controls the current movement of this character    
-    public Vector2 CurrentMovement { get; set; }
-
-    // Returns if this character can move normally (When dashing we can't)
-    public bool NormalMovement { get; set; }
-
-
-    // Internal
-    private Rigidbody2D myRigidbody2D;
-
-    // Start is called before the first frame update
-    void Start()
+    public class CharacterController : MonoBehaviour
     {
-        NormalMovement = true;
-        myRigidbody2D = GetComponent<Rigidbody2D>();
-    }
+        //[SerializeField] private float speed = 20f;  //REMOVE it, because we no longer use it anymore
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (NormalMovement)
+        // Controls the current movement of this character    
+        public Vector2 CurrentMovement { get; set; }
+
+        // Returns if this character can move normally (When dashing we can't)
+        public bool NormalMovement { get; set; }
+
+
+        // Internal
+        private Rigidbody2D _rigidbody2D;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            MoveCharacter();
+            NormalMovement = true;
+            _rigidbody2D = GetComponent<Rigidbody2D>();
         }
-    }
 
-    private void MoveCharacter()
-    {
-        Vector2 currentMovePosition = myRigidbody2D.position + CurrentMovement * Time.fixedDeltaTime;
-        myRigidbody2D.MovePosition(currentMovePosition);
-    }
-    //Extra Move in case we need it
-    public void MovePosition (Vector2 newposition)
-    {
-        myRigidbody2D.MovePosition(newposition);
-    }
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            if (NormalMovement)
+            {
+                MoveCharacter();
+            }
+        }
 
-    //Sets the current movement of our character
-    public void SetMovement (Vector2 newPosition)
-    {
-        CurrentMovement = newPosition;
+        private void MoveCharacter()
+        {
+            Vector2 currentMovePosition = _rigidbody2D.position + CurrentMovement * Time.fixedDeltaTime;
+            _rigidbody2D.MovePosition(currentMovePosition);
+        }
+
+        //Extra Move in case we need it
+        public void MovePosition(Vector2 newPosition)
+        {
+            _rigidbody2D.MovePosition(newPosition);
+        }
+
+        //Sets the current movement of our character
+        public void SetMovement(Vector2 newPosition)
+        {
+            CurrentMovement = newPosition;
+        }
     }
 }
