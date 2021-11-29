956ead5cbdfb6747f0f6bae4086505e0df478573
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2021-11-29 18:30:22 +0800

    fix: double projectile at one shoot

--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -4,34 +4,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
-{   
-    [Header("Settings")] 
-    [SerializeField] private float timeBtwShots = 0.5f;
+{
+    [Header("Settings")] [SerializeField] private float timeBtwShots = 10.5f;
 
-    [Header("Weapon")] 
-    [SerializeField] private bool useMagazine = true;
-    [SerializeField] private int magazineSize = 30;  // TODO
+    [Header("Weapon")] [SerializeField] private bool useMagazine = true;
+    [SerializeField] private int magazineSize = 30; // TODO
     [SerializeField] private bool autoReload = true;
 
-    [Header("Recoil")] 
-    [SerializeField] private bool useRecoil = true;
+    [Header("Recoil")] [SerializeField] private bool useRecoil = true;
     [SerializeField] private int recoilForce = 30;
-    
+
     public Character WeaponOwner { get; set; }
 
 
     public WeaponAmmo WeaponAmmo { get; set; }
-    
+
     public int CurrentAmmo { get; set; }
     public bool UseMagazine => useMagazine;
-    
-	public int MagazineSize => magazineSize;
-    
+
+    public int MagazineSize => magazineSize;
+
     public bool CanShoot { get; set; }
 
     // Internal
-    private float nextShotTime;
-    private CharacterController controller; 
+    private float allowNextShotTime;
+    private CharacterController controller;
 
     protected virtual void Awake()
     {
@@ -42,7 +39,7 @@ public class Weapon : MonoBehaviour
     protected virtual void Update()
     {
         WeaponCanShoot();
-        RotateWeapon();   
+        RotateWeapon();
     }
 
     public void TriggerShot()
@@ -57,77 +54,63 @@ public class Weapon : MonoBehaviour
             controller.ApplyRecoil(Vector2.one, 0f);
         }
     }
-    
+
     private void StartShooting()
     {
         if (useMagazine)
         {
-            if (WeaponAmmo != null)
+            if (WeaponAmmo == null) return;
+            if (WeaponAmmo.CanUseWeapon())
             {
-                if (WeaponAmmo.CanUseWeapon())
+                RequestShoot();
+            }
+            else
+            {
+                if (autoReload)
                 {
-                    RequestShot();
-                }
-                else
-                {
-                    if (autoReload)
-                    {
-                        Reload();
-                    }
+                    Reload();
                 }
             }
         }
         else
         {
-            RequestShot();
-		}
+            RequestShoot();
+        }
     }
 
-    protected virtual void RequestShot()
+    protected virtual void RequestShoot()
     {
-        if (!CanShoot)
-        {
-            return;
-        }
-
+        allowNextShotTime = Time.time + timeBtwShots;
         if (useRecoil)
         {
             Recoil();
         }
-         
-        WeaponAmmo.ConsumeAmmo();      
-	}
+
+        WeaponAmmo.ConsumeAmmo();
+    }
 
     private void Recoil()
     {
-        if (WeaponOwner != null)
-        {
-            if (WeaponOwner.GetComponent<CharacterFlip>().FacingRight)
-            {
-                controller.ApplyRecoil(Vector2.left, recoilForce);
-            }
-            else
-            {
-                controller.ApplyRecoil(Vector2.right, recoilForce);
-            }
-        }
+        if (WeaponOwner == null) return;
+        controller.ApplyRecoil(WeaponOwner.GetComponent<CharacterFlip>().FacingRight ? Vector2.left : Vector2.right,
+            recoilForce);
     }
 
     protected virtual void WeaponCanShoot()
     {
-        if (Time.time > nextShotTime)  
+        if (Time.time > allowNextShotTime)
         {
             CanShoot = true;
-            nextShotTime = Time.time + timeBtwShots;
+            // nextShotTime = Time.time + timeBtwShots;
         }
     }
 
     public void SetOwner(Character owner)
     {
-        WeaponOwner = owner; 
+        WeaponOwner = owner;
         controller = WeaponOwner.GetComponent<CharacterController>();
     }
-      
+
     public void Reload()
     {
         if (WeaponAmmo != null)
@@ -137,7 +120,7 @@ public class Weapon : MonoBehaviour
                 WeaponAmmo.RefillAmmo();
             }
         }
-	}
+    }
 
     protected virtual void RotateWeapon()
     {
