e1031a6f990d6a60411bb3dcd0b1741761506a1a
KeHu2266 <kehuang625@gmail.com>
2021-12-01 15:54:05 +0800

    Done initial damage trap

---
+++ Assets/Sprites/Component/Spike.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    [SerializeField] private int damage = 1;
+    
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerHealth>().Damage(1);
+        }
+    }
+}
+
