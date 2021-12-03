7492ef85dc0d7204fde85b26991fb27df93b1856
Denver <1139643975@qq.com>
2021-12-03 14:38:42 +0800

    Add Enemy Weapon. TODO: Fix ranged enemy

---
+++ Assets/Scripts/Character/Component/EnemyWeapon.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWeapon : CharacterComponents
+{
+    [Header("Weapon Settings")] [SerializeField]
+    private Weapon weaponToUse;
+
+    [SerializeField] private Transform weaponHolderPosition;
+
+    // Reference of the Weapon we are using
+    public Weapon CurrentWeapon { get; set; }
+
+    // Returns the reference to our Current Weapon Aim
+    public WeaponAim WeaponAim { get; set; }
+
+    private int mouseLeftBtn = 0;
+
+    protected override void Start()
+    {
+        base.Start();
+        EquipWeapon(weaponToUse, weaponHolderPosition);
+    }
+    
+
+    protected override void HandleInput()
+    {
+        base.HandleInput();
+        if (Input.GetMouseButton(mouseLeftBtn))
+        {
+            BeforeShoot();
+        }
+
+        if (Input.GetMouseButtonUp(mouseLeftBtn)) // If we stop shooting
+        {
+            StopWeapon();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reload();
+        }
+    }
+
+    public void BeforeShoot()
+    {
+        if (!CurrentWeapon.CanShoot) return;
+
+        CurrentWeapon.TriggerShot();
+        
+        if (character.CharacterType == Character.CharacterTypes.Player)
+        {
+            // FIXME
+            // TODO: weapon to UI canvas
+            UIManager.Instance.SetWeapon(CurrentWeapon.CurrentAmmo, CurrentWeapon.MagazineSize);
+        }
+    }
+
+    // When we stop shooting we stop using our Weapon
+    public void StopWeapon()
+    {
+        if (CurrentWeapon == null)
+        {
+            return;
+        }
+
+        CurrentWeapon.StopWeapon();
+    }
+
+    public void Reload()
+    {
+        if (CurrentWeapon == null)
+        {
+            return;
+        }
+
+        CurrentWeapon.Reload();
+        if (character.CharacterType == Character.CharacterTypes.Player)
+        {
+            UIManager.Instance.SetWeapon(CurrentWeapon.CurrentAmmo, CurrentWeapon.MagazineSize);
+        }
+    }
+
+    public void EquipWeapon(Weapon weapon, Transform weaponPosition)
+    {
+        CurrentWeapon = Instantiate(weapon, weaponPosition.position, weaponPosition.rotation);
+        CurrentWeapon.transform.parent = weaponPosition;
+        CurrentWeapon.SetOwner(character);
+        WeaponAim = CurrentWeapon.GetComponent<WeaponAim>();
+
+        if (character.CharacterType == Character.CharacterTypes.Player)
+        {
+            UIManager.Instance.SetWeapon(CurrentWeapon.CurrentAmmo, CurrentWeapon.MagazineSize);
+        }
+    }
+}
