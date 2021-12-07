fed384805306ff103e564914a29a4c9bfa4dcca4
Denver <1139643975@qq.com>
2021-12-07 16:08:28 +0800

    Fix Boss Projectile

--- Assets/Scripts/Weapon/Enemy/Pool/BossReturnToPool.cs
+++ Assets/Scripts/Weapon/Enemy/Pool/BossReturnToPool.cs
@@ -1,15 +1,11 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using UnityEngine;
 
-public class ReturnToPool : MonoBehaviour
+public class BossReturnToPool : MonoBehaviour
 {
-    [Header("Settings")] 
-    [SerializeField] private LayerMask WallMask;
+    [Header("Settings")]
     [SerializeField] private LayerMask EnemyMask;
-    [SerializeField] private LayerMask BossMask;
     [SerializeField] private float lifeTime = 2f;
     
     [Header("Effects")]
@@ -36,30 +32,7 @@ public class ReturnToPool : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (CheckLayer(other.gameObject.layer, WallMask))
-        {
-            if (projectile != null)
-            {
-                projectile.DisableProjectile();
-            }
-            impactPS.Play();
-            AudioManager.Instance.PlayOneShot(AudioEnum.ProjectileHitWall);
-            Invoke(nameof(Return), impactPS.main.duration);
-
-        }
-
         if (CheckLayer(other.gameObject.layer, EnemyMask))
-        {
-            if (projectile != null)
-            {
-                projectile.DisableProjectile();
-            }
-
-            impactPS.Play();
-            Invoke(nameof(Return), impactPS.main.duration);
-        }
-        
-        if (CheckLayer(other.gameObject.layer, BossMask))
         {
             if (projectile != null)
             {
