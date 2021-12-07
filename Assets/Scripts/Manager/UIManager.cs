a2dc20ec108498c41876cdbfe7c6d323fcbf2639
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2021-12-08 01:49:03 +0800

    fix: bars fill rate

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -1,5 +1,7 @@
+using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class UIManager : Singleton<UIManager>
@@ -7,12 +9,11 @@ public class UIManager : Singleton<UIManager>
     private int currentAmmo;
     private Image healthBar;
     private Image healthBarDelay;
-    [SerializeField] private float amountDelayRate = 0.0005f;
-    [SerializeField] private float ShieldamountDelayRate;
-    [Header("Weapon")]
-    [SerializeField] private Image weaponImage;
+    [SerializeField] private float healthAmountDelayRate;
+    [SerializeField] private float shieldAmountDelayRate;
+    [Header("Weapon")] [SerializeField] private Image weaponImage;
 
-    [SerializeField] private GameObject weaponAmmoNum;
+    [SerializeField] private GameObject weaponAmmoUI;
 
 
     private float playerCurrentHealth;
@@ -28,7 +29,6 @@ public class UIManager : Singleton<UIManager>
     {
         // Initialize status bars
         var statusBarsContainer = GameObject.Find("StatusBarsContainer").transform;
-        // TODO FIXME
         healthBar = statusBarsContainer.Find("HealthBarContainer").Find("HealthBar").GetComponent<Image>();
         shieldBar = statusBarsContainer.Find("ShieldBarContainer").Find("ShieldBar").GetComponent<Image>();
         healthBarDelay = statusBarsContainer.Find("HealthBarContainer").Find("HealthBarDelay").GetComponent<Image>();
@@ -41,7 +41,7 @@ public class UIManager : Singleton<UIManager>
     }
 
 
-    public void SetUIStates(float hp, float maxHp, float shield,
+    public void SetUIPlayerStates(float hp, float maxHp, float shield,
         float maxShield)
     {
         playerCurrentHealth = hp;
@@ -50,34 +50,44 @@ public class UIManager : Singleton<UIManager>
         playerMaxShield = maxShield;
     }
 
-    public void SetWeapon(string current, string magazineSize)
+    public void SetUIWeaponAmmo(string current, string magazineSize)
     {
         // // TODO: Weapon Switch
         // currentAmmo = current;
         // totalAmmo = magazineSize;
-        weaponAmmoNum.GetComponent<TextMeshProUGUI>().text = current + "/" + magazineSize;
+        weaponAmmoUI.GetComponent<TextMeshProUGUI>().text = current + "/" + magazineSize;
     }
 
-    private void UpdateBars()
-    {
-        // TODO: Mathf.lerp
-        healthBar.fillAmount = playerCurrentHealth / playerMaxHealth;
-        if (healthBarDelay.fillAmount > healthBar.fillAmount)
-            healthBarDelay.fillAmount -= amountDelayRate;
-        else
-            healthBarDelay.fillAmount = healthBar.fillAmount;
-        shieldBar.fillAmount = playerCurrentShield / playerMaxShield;
-
-        if (shieldBarDelay.fillAmount > shieldBar.fillAmount)
-            shieldBarDelay.fillAmount -= ShieldamountDelayRate;
-        else
-            shieldBarDelay.fillAmount = shieldBar.fillAmount;
-    }
-    public void UpdateWeaponSprite(Sprite weaponSprite)
+    public void SetUIWeaponSprite(Sprite weaponSprite)
     {
         weaponImage.sprite = weaponSprite;
         weaponImage.SetNativeSize();
     }
 
+    private void UpdateBars()
+    {
+        healthBar.fillAmount = playerCurrentHealth / playerMaxHealth;
+        
+        
 
+        if (healthBarDelay.fillAmount > healthBar.fillAmount)
+        {
+            healthBarDelay.fillAmount -= healthAmountDelayRate * Time.deltaTime;
+        }
+        else
+        {
+            healthBarDelay.fillAmount = healthBar.fillAmount;
+        }
+
+        shieldBar.fillAmount = playerCurrentShield / playerMaxShield;
+
+        if (shieldBarDelay.fillAmount > shieldBar.fillAmount)
+        {
+            shieldBarDelay.fillAmount -= shieldAmountDelayRate * Time.deltaTime;
+        }
+        else
+        {
+            shieldBarDelay.fillAmount = shieldBar.fillAmount;
+        }
+    }
 }
