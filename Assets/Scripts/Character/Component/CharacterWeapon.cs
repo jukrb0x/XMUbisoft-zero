c427cfc61e54b9d93f04f3d549ff82a38710de37
Denver <1139643975@qq.com>
2021-12-07 10:19:09 +0800

    Fix Boss Prefabs and Enemy Attack Effects

--- Assets/Scripts/Character/Component/CharacterWeapon.cs
+++ Assets/Scripts/Character/Component/CharacterWeapon.cs
@@ -116,7 +116,7 @@ public class CharacterWeapon : CharacterComponents
         if (character.CharacterType == Character.CharacterTypes.Player)
         {
             UIManager.Instance.SetWeapon(CurrentWeapon.CurrentAmmo, CurrentWeapon.MagazineSize);
-            UIManager.Instance.UpdateWeaponSprite(CurrentWeapon.gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite);
+            // UIManager.Instance.UpdateWeaponSprite(CurrentWeapon.gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite);
         }
     }
 }
