223d323a2e01b1c978d239aa9172a72cff7563e0
Denver <1139643975@qq.com>
2021-12-07 11:39:19 +0800

    Fix Ranged Enemy Shot Gun

--- Assets/Scripts/Weapon/WeaponAim.cs
+++ Assets/Scripts/Weapon/WeaponAim.cs
@@ -1,40 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponAim : MonoBehaviour
 {
     [SerializeField] private GameObject reticlePrefab;
-    private Vector3 currentAim = Vector3.zero;
-    private Vector3 currentAimAbsolute = Vector3.zero;
-
-    private Vector3 direction;
-    private Quaternion initialRotation;
-    private Quaternion lookRotation;
-
-    private Camera mainCamera;
-    private Vector3 mousePosition;
-    private GameObject reticle;
-    private Vector3 reticlePosition;
-    private Weapon weapon;
 
     // Returns the current absolute angle
     public float CurrentAimAngleAbsolute { get; set; }
-
+    
     // Returns the current angle
     public float CurrentAimAngle { get; set; }
 
+    private Camera mainCamera;
+    private GameObject reticle;
+    private Weapon weapon;
+
+    private Vector3 direction;
+    private Vector3 mousePosition;
+    private Vector3 reticlePosition;
+    private Vector3 currentAim = Vector3.zero;
+    private Vector3 currentAimAbsolute = Vector3.zero;
+    private Quaternion initialRotation;
+    private Quaternion lookRotation;
+
     private void Start()
     {
-        Cursor.visible = false;
+        Cursor.visible = false; 
         weapon = GetComponent<Weapon>();
-        initialRotation = transform.rotation;
-
+        initialRotation = transform.rotation;  
+     
         mainCamera = Camera.main;
         reticle = Instantiate(reticlePrefab);
     }
 
     private void Update()
-    {
-        GetMousePosition();
+	{
+        if (weapon.WeaponOwner.CharacterType == Character.CharacterTypes.Player)
+        {
+            GetMousePosition();
+        }
+        else
+        {
+            EnemyAim();
+        }
+
         MoveReticle();
         RotateWeapon();
     }
@@ -44,7 +55,7 @@ public class WeaponAim : MonoBehaviour
     {
         // Get Mouse Position
         mousePosition = Input.mousePosition;
-        mousePosition.z = 5f; // We set this value to ensure the camera always stays infront to view everything in game
+        mousePosition.z = 5f;  // We set this value to ensure the camera always stays infront to view everything in game
 
         // Get World space position
         direction = mainCamera.ScreenToWorldPoint(mousePosition);
@@ -53,16 +64,15 @@ public class WeaponAim : MonoBehaviour
 
         currentAimAbsolute = direction - transform.position;
         if (weapon.WeaponOwner.GetComponent<CharacterFlip>().FacingRight)
+        {
             currentAim = direction - transform.position;
+        }
         else
+        {
             currentAim = transform.position - direction;
+        }
     }
 
-    public void SetAim(Vector2 newAim)
-    {
-        currentAim = newAim;
-    }
-    
     public void RotateWeapon()
     {
         if (currentAim != Vector3.zero && direction != Vector3.zero)
@@ -73,19 +83,35 @@ public class WeaponAim : MonoBehaviour
 
             // Clamp our rotation
             if (weapon.WeaponOwner.GetComponent<CharacterFlip>().FacingRight)
+            {
                 CurrentAimAngle = Mathf.Clamp(CurrentAimAngle, -180, 180);
+            }
             else
+            {
                 CurrentAimAngle = Mathf.Clamp(CurrentAimAngle, -180, 180);
-
+            }
+            
             // Apply the angle
             lookRotation = Quaternion.Euler(CurrentAimAngle * Vector3.forward);
             transform.rotation = lookRotation;
         }
         else
         {
-            CurrentAimAngle = 0f; // If the mouse is not moving at all at the beginning
+            CurrentAimAngle = 0f;  // If the mouse is not moving at all at the beginning
             transform.rotation = initialRotation;
         }
+	}
+
+    private void EnemyAim()
+    {
+        currentAimAbsolute = currentAim;
+        currentAim = weapon.WeaponOwner.GetComponent<CharacterFlip>().FacingRight ? currentAim : -currentAim;
+        direction = currentAim - transform.position;
+    }
+
+    public void SetAim(Vector2 newAim)
+    {
+        currentAim = newAim;
     }
 
     // Moves our reticle towards our Mouse Position
@@ -94,9 +120,9 @@ public class WeaponAim : MonoBehaviour
         reticle.transform.rotation = Quaternion.identity; //set the normal rotation
         reticle.transform.position = reticlePosition;
     }
+
     public void DestroyReticle()
     {
         Destroy(reticle.gameObject);
     }
-
 }
